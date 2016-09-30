cab86378678a92c72991e3f030b03586ab641a3a
Yaseen Lotfi <yalotfi@outlook.com>
2016-09-30 12:05:05 -0400

    Rename to CARTmodel and remove extra code

--- CARTmodel_01_.R
+++ CARTmodel_01_.R
@@ -30,22 +30,3 @@ pred   <- predict(fit, test, type = "class")
 submit.tree <- data.frame(PassengerId = test$PassengerId, Survived = pred)
 write.csv(submit.tree, "CARTmodel.csv", row.names = F) ## 78.469%
 rm(submit.tree)
-
-
-#########################
-## Feature Engineering ##
-#########################
-## Recombine test and train sets
-test$Survived <- NA
-combine <- rbind(train, test)
-
-## Get sense of missing data, mostly in Age
-apply(train, 2, function(x) (sum(is.na(x))))
-ageNA <- subset(train, is.na(train$Age) == T)
-table(ageNA$Survived)  # 29.37% passengers of unknown age survived
-table(train$Survived)  # as opposed to general survival rate of ~38.38%
-
-## Explore Fares
-summary(combine$Fare)
-
-
