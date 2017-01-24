8377cbfb480381bd1fae6122cdeac8a1ed406a94
Yaseen Lotfi <yalotfi@outlook.com>
2017-01-24 16:31:06 -0500

    Start work on Random Forest Classifier

---
+++ python_scripts/random_forest_classifier.py
@@ -0,0 +1,20 @@
+'''
+Author: Yaseen Lotfi
+Title: Random Forest Classifier
+Description: Using a Random Forest Classifier, how well can we predict
+    whether or not a passanger of the Titanic survived or not? 
+'''
+
+
+# Import dependencies
+import feature_engineering as FE
+from sklearn.ensemble import RandomForestClassifier as rf
+
+
+def main():
+    # Main logic for Random Forests Classifier
+    print('Hello Random Forests')
+
+
+if __name__ == '__main__':
+    main()
