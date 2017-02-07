21d814e813bb90134a1a09e4f99522c4bd243da7
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-06 22:13:52 -0500

    simplify routing

--- D3_explore/app.js
+++ D3_explore/app.js
@@ -1,21 +1,18 @@
 //Require modules
 var d3 = require('d3');
-//var dimple = require('dimple');
-var path = require('path');
 var express = require('express');
 var app = express();
+var path = require('path');
 
-//Other variables
-var port = process.env.PORT || 3000
-var indexHTML = '/index.html'
-
-//app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + indexHTML);
+    res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(port, function() {
-    console.log("Server listening at http:%s", port)
+var server = app.listen(process.env.PORT || 5000, function() {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Server listening at http://%s:%s", host, port)
 })
